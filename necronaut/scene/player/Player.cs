3aa6efea31d41544de1e43cca8590c90911642d5
ggeirby <roromanenko2004@gmail.com>
2025-02-05 20:40:38 +0200

    Add platform brash

--- necronaut/scene/player/Player.cs
+++ necronaut/scene/player/Player.cs
@@ -3,112 +3,112 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	private const float Speed = 300.0f;
-	private const float JumpVelocity = -400.0f;
-	private float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-	private float direction = 0;
-	private bool isJumped = false;
-	private bool isPunch = false;
-	private float _lastDirection = -1;
-	[Export] private float _rayLength = 100f;
-	private AnimatedSprite2D _sprite;
+  private const float Speed = 300.0f;
+  private const float JumpVelocity = -400.0f;
+  private float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+  private float direction = 0;
+  private bool isJumped = false;
+  private bool isPunch = false;
+  private float _lastDirection = -1;
+  [Export] private float _rayLength = 100f;
+  private AnimatedSprite2D _sprite;
 
-	public override void _Ready()
-	{
-		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		_sprite.AnimationFinished += OnAnimationFinished;
-	}
+  public override void _Ready()
+  {
+	_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	_sprite.AnimationFinished += OnAnimationFinished;
+  }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		HandleMovement();
-		HandleJump();
-		MoveAndSlide();
-		if (Input.IsActionJustPressed("jump"))
-			PerformRaycast();
-		HandleAnimation();
-	}
+  public override void _PhysicsProcess(double delta)
+  {
+	HandleMovement();
+	HandleJump();
+	MoveAndSlide();
+	if (Input.IsActionJustPressed("jump"))
+	  PerformRaycast();
+	HandleAnimation();
+  }
 
-	private void HandleMovement()
+  private void HandleMovement()
+  {
+	Vector2 velocity = Velocity;
+	if (!IsOnFloor())
+	  velocity.Y += Gravity * (float)GetProcessDeltaTime();
+	direction = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+	if (direction != 0)
 	{
-		Vector2 velocity = Velocity;
-		if (!IsOnFloor())
-			velocity.Y += Gravity * (float)GetProcessDeltaTime();
-		direction = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-		if (direction != 0)
-		{
-			velocity.X = direction * Speed;
-			if ((_lastDirection < 0 && direction > 0) || (_lastDirection > 0 && direction < 0))
-			{
-				_lastDirection = direction;
-				_sprite.FlipH = !_sprite.FlipH;
-			}
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-		Velocity = velocity;
+	  velocity.X = direction * Speed;
+	  if ((_lastDirection < 0 && direction > 0) || (_lastDirection > 0 && direction < 0))
+	  {
+		_lastDirection = direction;
+		_sprite.FlipH = !_sprite.FlipH;
+	  }
 	}
+	else
+	{
+	  velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+	}
+	Velocity = velocity;
+  }
 
-	private void HandleJump()
+  private void HandleJump()
+  {
+	if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 	{
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			Velocity = new Vector2(Velocity.X, JumpVelocity);
-			isJumped = true;
-		}
-		else if (IsOnFloor())
-		{
-			isJumped = false;
-		}
+	  Velocity = new Vector2(Velocity.X, JumpVelocity);
+	  isJumped = true;
 	}
+	else if (IsOnFloor())
+	{
+	  isJumped = false;
+	}
+  }
 
-	private void HandleAnimation()
+  private void HandleAnimation()
+  {
+	if (_sprite == null) return;
+	if (isJumped)
 	{
-		if (_sprite == null) return;
-		if (isJumped)
-		{
-			if (Velocity.Y <= 0)
-				_sprite.Play("jump");
-			else
-				_sprite.Play("fall");
-		}
-		else if (direction != 0)
-		{
-			_sprite.Play("walk");
-		}
-		else if (!isPunch)
-		{
-			_sprite.Play("idle");
-		}
-		if (isPunch)
-		{
-			_sprite.Play("attack");
-		}
+	  if (Velocity.Y <= 0)
+		_sprite.Play("jump");
+	  else
+		_sprite.Play("fall");
 	}
+	else if (direction != 0)
+	{
+	  _sprite.Play("run");
+	}
+	else if (!isPunch)
+	{
+	  _sprite.Play("idle");
+	}
+	if (isPunch)
+	{
+	  _sprite.Play("attack");
+	}
+  }
 
-	private void PerformRaycast()
+  private void PerformRaycast()
+  {
+	isPunch = true;
+	Vector2 start = GlobalPosition;
+	Vector2 end = start + new Vector2(_lastDirection * _rayLength, 0);
+	var spaceState = GetWorld2D().DirectSpaceState;
+	var query = PhysicsRayQueryParameters2D.Create(start, end);
+	var result = spaceState.IntersectRay(query);
+	if (result.Count > 0)
 	{
-		isPunch = true;
-		Vector2 start = GlobalPosition;
-		Vector2 end = start + new Vector2(_lastDirection * _rayLength, 0);
-		var spaceState = GetWorld2D().DirectSpaceState;
-		var query = PhysicsRayQueryParameters2D.Create(start, end);
-		var result = spaceState.IntersectRay(query);
-		if (result.Count > 0)
-		{
-			Node collider = result["collider"].As<Node>();
-			if (collider != null)
-			{
-			}
-		}
+	  Node collider = result["collider"].As<Node>();
+	  if (collider != null)
+	  {
+	  }
 	}
+  }
 
-	private void OnAnimationFinished()
-	{
-		if (_sprite.Animation == "attack")
-			isPunch = false;
-			_sprite.Play("idle");
-	}
+  private void OnAnimationFinished()
+  {
+	if (_sprite.Animation == "attack")
+	  isPunch = false;
+	  _sprite.Play("idle");
+  }
 }
