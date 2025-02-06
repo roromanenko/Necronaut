b8cb34370a34cafaef9f7d3a04ac20c82b5bf317
Alex <108016826+0Reach0@users.noreply.github.com>
2025-02-06 15:21:30 +0200

    Update player script

--- necronaut/scene/player/Player.cs
+++ necronaut/scene/player/Player.cs
@@ -3,34 +3,42 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-  private const float Speed = 300.0f;
-  private const float JumpVelocity = -500.0f;
-  private float Gravity = 2000f;
-  private float direction = 0;
-  private bool isJumped = false;
-  private bool isPunch = false;
-  private float _lastDirection = -1;
-  [Export] private float _rayLength = 100f;
-  private AnimatedSprite2D _sprite;
+	private const float Speed = 300.0f;
+	private const float JumpVelocity = -500.0f;
+	private const float Gravity = 1000f;
 
-  public override void _Ready()
-  {
-	_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	_sprite.AnimationFinished += OnAnimationFinished;
-  }
+ private float direction = 0;
+	private float _lastDirection = -1;
+	private bool _isJumped = false;
+	private bool _isPunch = false;
 
-  public override void _PhysicsProcess(double delta)
-  {
-	HandleMovement();
-	HandleJump();
-	MoveAndSlide();
-	if (Input.IsActionJustPressed("jump"))
-	  PerformRaycast();
-	HandleAnimation();
-  }
+	[Export] private float _rayLength = 100f;
+	private AnimatedSprite2D _sprite;
 
-  private void HandleMovement()
-  {
+	public override void _Ready()
+	{
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_sprite.AnimationFinished += OnAnimationFinished;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		ApplyGravity(delta);
+		HandleMovement();
+		HandleJump();
+		MoveAndSlide();
+		if (Input.IsActionJustPressed("jump")) PerformRaycast();
+		HandleAnimation();
+	}
+
+	private void ApplyGravity(double delta)
+{
+	if (!IsOnFloor())
+		Velocity += new Vector2(0, Gravity * (float)delta);
+}
+
+	private void HandleMovement()
+	{
 	Vector2 velocity = Velocity;
 	if (!IsOnFloor())
 	  velocity.Y += Gravity * (float)GetProcessDeltaTime();
@@ -49,66 +57,53 @@ public partial class Player : CharacterBody2D
 	  velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 	}
 	Velocity = velocity;
-  }
+	}
 
-  private void HandleJump()
-  {
-	if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+	private void HandleJump()
 	{
-	  Velocity = new Vector2(Velocity.X, JumpVelocity);
-	  isJumped = true;
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			Velocity = new Vector2(Velocity.X, JumpVelocity);
+			_isJumped = true;
+		}
+		else if (IsOnFloor())
+		{
+			_isJumped = false;
+		}
 	}
-	else if (IsOnFloor())
-	{
-	  isJumped = false;
-	}
-  }
 
-  private void HandleAnimation()
-  {
-	if (_sprite == null) return;
-	if (isJumped)
+	private void HandleAnimation()
 	{
-	  if (Velocity.Y <= 0)
-		_sprite.Play("jump");
-	  else
-		_sprite.Play("fall");
-	}
-	else if (direction != 0)
-	{
-	  _sprite.Play("run");
-	}
-	else if (!isPunch)
-	{
-	  _sprite.Play("idle");
-	}
-	if (isPunch)
-	{
-	  _sprite.Play("attack");
-	}
-  }
+		if (_sprite == null) return;
 
-  private void PerformRaycast()
-  {
-	isPunch = true;
-	Vector2 start = GlobalPosition;
-	Vector2 end = start + new Vector2(_lastDirection * _rayLength, 0);
-	var spaceState = GetWorld2D().DirectSpaceState;
-	var query = PhysicsRayQueryParameters2D.Create(start, end);
-	var result = spaceState.IntersectRay(query);
-	if (result.Count > 0)
-	{
-	  Node collider = result["collider"].As<Node>();
-	  if (collider != null)
-	  {
-	  }
+		if (_isPunch)
+			_sprite.Play("attack");
+		else if (_isJumped)
+			_sprite.Play(Velocity.Y <= 0 ? "jump" : "fall");
+		else if (Velocity.X != 0)
+			_sprite.Play("run");
+		else
+			_sprite.Play("idle");
 	}
-  }
 
-  private void OnAnimationFinished()
-  {
-	if (_sprite.Animation == "attack")
-	  isPunch = false;
-	  _sprite.Play("idle");
-  }
+	private void PerformRaycast()
+	{
+		_isPunch = true;
+		Vector2 start = GlobalPosition;
+		Vector2 end = start + new Vector2(_lastDirection * _rayLength, 0);
+		var spaceState = GetWorld2D().DirectSpaceState;
+		var query = PhysicsRayQueryParameters2D.Create(start, end);
+		var result = spaceState.IntersectRay(query);
+
+		//if (result.Count > 0 && result["collider"] is Node collider)
+		//{
+			//// Обработка попадания
+		//}
+	}
+
+	private void OnAnimationFinished()
+	{
+		if (_sprite.Animation == "attack")
+			_isPunch = false;
+	}
 }
